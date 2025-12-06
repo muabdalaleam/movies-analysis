f0217c24b4d9e0260530c33bda26dc132183640c
muabdalaleam <muhammed.abdelaleam@gmail.com>
2025-12-06 09:06:30 +0200

    Writed a new minig script for the TMDB API

--- scripts/mining.py
+++ scripts/mining.py
@@ -1,62 +1,64 @@
-from utils import fetch_env
-import requests
 import json
-# import sqlite3
+import time
+from utils import fetch_env, retrying_request
 
-MAX_PAGE = 500
+MAX_PAGE = 1 # 20 movies per page
 TIMEOUT = 10
+MAX_RETRIES = 3
+RETRY_WAIT = 0.2 # seconds
 
 api_key = fetch_env('TMBD_API_KEY')
 headers = {
-    "accept": "application/json",
+    "accept":         "application/json",
     "Authorization": f"Bearer {api_key}"
 }
 
-# movies_data = []
-# 
-# for page in range(1, MAX_PAGE + 1):
-#     discover_movies_url = (
-#         f'https://api.themoviedb.org/3/discover/movie?include_adult=false'
-#         f'&include_video=false&language=en-US&page={page}&sort_by=popularity.desc'
-#     )
-# 
-#     discover_movies_response = requests.get(discover_movies_url, headers=HEADERS)
-# 
-#     for movie in discover_movies_response['results']:
-#         movie_id               : str  = movie['id']
-#         movie_details_url      : str  = f"https://api.themoviedb.org/3/movie/{movie_id}?language=en-US"
-#         movie_details_response : dict = requests.get(movie_details_url, headers=HEADERS).json()
-# 
-#         movie_data = {
-#             'movie_id'             : movie_details_response['id'],
-#             'movie_title'          : movie_details_response['original_title'],
-#             'overview'             : movie_details_response['overview'],
-#             'popularity'           : movie_details_response['popularity'],
-#             'rating'               : movie_details_response['vote_average'],
-#             'tagline'              : movie_details_response['tagline'],
-#             'runtime'              : movie_details_response['runtime'],
-#             'revenue'              : movie_details_response['revenue'],
-#             'release_date'         : movie_details_response['release_date'],
-#             'vote_count'           : movie_details_response['vote_count'],
-#             'budget'               : movie_details_response['budget'],
-#             'genres'               : str([d['name'] for d in movie_details_response['genres']]),
-#             'production_companies' : str([d['name'] for d in movie_details_response['production_companies']])
-#         }
-# 
-#         movies_data.append(movie_data)
-# 
-# 
-if __name__ == "__main__":
-
-    print(api_key)
+def search_movies(page: int) -> list[str]:
     url = (
         f"https://api.themoviedb.org/3/discover/movie?include_adult=false"
-        f"&include_video=false&language=en-US&page={1}&sort_by=vote_count.desc"
+        f"&include_video=false&language=en-US&page={page}&sort_by=vote_count.desc"
     )
-    response = requests.get(url, headers=headers, timeout=TIMEOUT)
-    print(response.json()["results"][0])
+    response = retrying_request(url, headers, TIMEOUT, MAX_RETRIES, RETRY_WAIT,
+                                raise_on_fail=False)
 
-    # with sqlite3.connect("./data/movies.db") as con:
-    #     conn = sqlite3.connect('../database.db')
-    #     movies_data.write_csv('raw-data/movies_data.csv')
-    #     movies_data.to_pandas().to_sql('movies_data', con, if_exists='replace', index=False)
+    if response is None:
+        return None
+
+    ids = [movie["id"] for movie in response.json()["results"]]
+
+    return ids
+
+def scrape_movie(id: str) -> dict:
+    url    = f"https://api.themoviedb.org/3/movie/{id}"
+    response = retrying_request(url, headers, TIMEOUT, MAX_RETRIES, RETRY_WAIT,
+                                raise_on_fail=False)
+
+    if response is None:
+        return None
+
+    return response.json()
+
+
+if __name__ == "__main__":
+    movies = []
+    movie = None
+
+    for i in range(MAX_PAGE):
+        movie_ids = search_movies(i+1)
+        print(f"Scraping the {i+1}th page of movies from the API with {len(movie_ids)} movies.")
+
+        time.sleep(0.1)
+
+        if not movie_ids or (not movie and i > 0):
+            break
+
+        for id in movie_ids:
+            movie = scrape_movie(id)
+            time.sleep(0.1) # respecting TMDB's api
+            movies.append(movie)
+
+            if not movie:
+                break
+
+    with open("./data/movies_raw.json", "w") as f:
+        json.dump(movies, f)
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,16 @@
 import re
+import time
+import requests
+from typing import Optional
 
 ENV_PATH = "./.env"
 
 def fetch_env(key: str) -> str:
+    """
+    My shitty .env parser
+    params: a key as a string
+    returns: the value of that key as a string always
+    """
     with open(ENV_PATH, "r") as f:
         val             = ""
         env_content     = f.read()
@@ -25,3 +33,26 @@ def fetch_env(key: str) -> str:
             raise ValueError(f"{val} not found in the {ENV_PATH} file in the root of the project.")
 
     return val
+
+def retrying_request(url, headers, timeout, max_retries, retry_wait=0,
+                     raise_on_fail=True) -> Optional[requests.Response]:
+    """
+    a request method for paranoid people
+    """
+    retries = 0
+    while retries < max_retries:
+        try:
+            response = requests.get(url, headers=headers, timeout=timeout)
+            response.raise_for_status()
+            break
+        except (requests.ConnectionError, requests.RequestException) as e:
+            retries += 1
+            time.sleep(retry_wait)
+
+            print(f"Failed requesting {url} after {retries-1} retires.")
+            if retries >= max_retries:
+                if    raise_on_fail: raise e
+                else: return None
+                
+
+    return response
